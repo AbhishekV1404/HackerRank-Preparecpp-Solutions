4ce23f75e0d8a40d7c96854b07579562bd3eeae3
Abhishek Verma <83875167+AbhishekV1404@users.noreply.github.com>
2023-04-04 00:59:54 +0530

    Create ExceptionalServer.cpp

---
+++ ExceptionalServer.cpp
@@ -0,0 +1,20 @@
+
+
+		/* Enter your code here. */
+        try {
+        if (A > 1073741823LL) {
+            Server::compute(2, 1);
+            throw bad_alloc();
+        }
+        cout << Server::compute(A, B) << '\n';
+    }
+    catch (bad_alloc &e) {
+        cout << "Not enough memory\n";
+    }
+    catch (exception &e) {
+        cout << "Exception: " << e.what() << '\n';
+    }
+    catch (...) {
+        cout << "Other Exception\n";
+    }
+
